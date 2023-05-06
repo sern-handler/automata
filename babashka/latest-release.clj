28c585f0deaa0438910238aabacd627e7962ee7e
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-05-06 11:45:51 -0500

    chore: include auth

--- babashka/latest-release.clj
+++ babashka/latest-release.clj
@@ -1,8 +1,17 @@
 (require '[babashka.http-client :as http]
          '[cheshire.core :as json])
 
+(def token (get env "TOKEN"))
+
 (defn latest-version [sern-repo]
-  (-> (http/get (str "https://api.github.com/repos/sern-handler/" sern-repo "/tags"))
+  (-> (http/get 
+        (str "https://api.github.com/repos/sern-handler/" sern-repo "/tags") 
+        {:headers 
+         {"Accept" "application/vnd.github+json"}
+         {"X-GitHub-Api-Version" "2022-11-28"}
+         {"Authorization" (str "Bearer " token)}
+         }
+        )
       :body
       (json/parse-string true)
       first
