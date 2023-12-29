e9dfe6c43986f224fa2058118ff64d05f02bc0bf
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2023-12-29 20:12:36 +0000

    fix: forgot to add nextauthprovider

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import NextAuthProvider from "./NextAuthProvider.jsx";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable}`}>
+        <NextAuthProvider>
+          {children}
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
