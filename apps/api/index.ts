691264da5628da534552619a26e50a517d892074
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2024-03-18 21:24:49 +0100

    Update index.ts

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -257,7 +257,7 @@ app.get('/ping', (req, res) => {
 	res.send('Pong')
 })
 
-const port = process.env.PORT || 4000
+const port = Number(process.env.PORT!) || 4000
 app.listen(port, '0.0.0.0', () => {
 	console.log(`Server listening on [::]${port}`)
 })
